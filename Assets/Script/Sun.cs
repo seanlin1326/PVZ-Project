ee3669a20e421c735043bcf415a87cc12e9f443d
Sean Lin Hsuan yu <seanlin1326@gmail.com>
2021-07-30 20:52:17 +0800

    初始還境

---
+++ Assets/Script/Sun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    //下落的目標PosY
+    private float downTargetPosY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //當陽光從天空初始化的方法
+    public void InitForSky(float _downTargetPosY,float _createPosX,float _createPosY)
+    {
+        downTargetPosY = _downTargetPosY;
+        transform.position = new Vector2(_createPosX, _createPosY);
+    }
+}
---
+++ Assets/Script/SunSkyManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSkyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject sunPrefab;
+    //創建陽光時候的座標Y
+   [SerializeField]private float createSunPosY=6f;
+
+    //創建陽光時 最左和最右的X軸座標，在這個範圍內隨機
+   [SerializeField]private float createSunMinPosX = -4f;
+   [SerializeField]private float createSunMaxPosX = 6.6f;
+
+    //陽光下落時 最高和最低的目的地Y軸
+    [SerializeField]private float sunDownMinPosY = -3.7f;
+    [SerializeField]private float sunDownMaxPosY = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sunPrefab = Resources.Load<GameObject>("Prefab/Sun");
+        InvokeRepeating(nameof(CreateSun), 3f, 3f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    //在空中生成陽光
+    void CreateSun()
+    {
+        GameObject _spawnSunObj = Instantiate(sunPrefab, Vector3.zero, Quaternion.identity, transform);
+    }
+}
